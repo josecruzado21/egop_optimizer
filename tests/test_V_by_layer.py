57592190edcfc978dc16ba570833f649ee58a167
Adela DePavia <adela.depavia@yale.edu>
2026-03-11 22:42:45 -0500

    implemented tests on CNNs

--- tests/test_V_by_layer.py
+++ tests/test_V_by_layer.py
@@ -1,21 +1,26 @@
 import torch
 import unittest
 
-from egop_optimizer.engine.train import basic_train_loop
 from egop_optimizer.utils.EGOP_utils import compute_V_by_layer
+
 from egop_optimizer.models.TinyMNISTClassifier import TinyMNISTClassifier
 from egop_optimizer.dataloaders.tinyMNIST_dataloader import tinyMNIST_dataloader
+from egop_optimizer.models.CifarClassifier import TinyCifarClassifier
+from test_TinyCifarClassifier import load_cifar_debugging_subset
 
 import pdb
 
 """
 TODO'S:
 -test CNN
+    - only basic computation tested, no shapes
+-test CNN when reparameterizing linear layers
 -test shapes when using randomized SVD
+-test shapes when using n_components
 """
 
 
-def get_V_dict_for_tests(OG_model, use_randomized_svd):
+def get_V_dict_for_TinyMNIST(OG_model, use_randomized_svd):
     trainloader, _, _ = tinyMNIST_dataloader(batch_size=128)
     criterion = torch.nn.CrossEntropyLoss(reduction="mean")
 
@@ -28,14 +33,30 @@ def get_V_dict_for_tests(OG_model, use_randomized_svd):
     )
 
 
+def get_V_dict_for_Cifar10(
+    OG_model, use_randomized_svd=False, reparam_linear_layers=False
+):
+    trainloader, _, _ = load_cifar_debugging_subset()
+    criterion = torch.nn.CrossEntropyLoss(reduction="mean")
+
+    return compute_V_by_layer(
+        model_OG=OG_model,
+        k=100,
+        data_loader=trainloader,
+        criterion=criterion,
+        use_randomized_svd=use_randomized_svd,
+        reparam_linear_layers=reparam_linear_layers,
+    )
+
+
 class TestLinearLayersBasic(unittest.TestCase):
     def test_computation_executes(self):
         """
         Tests that we can run compute_V_by_layer without error
         """
         model = TinyMNISTClassifier(input_size=64, hidden_sizes=[10])
-        get_V_dict_for_tests(OG_model=model, use_randomized_svd=False)
-        get_V_dict_for_tests(OG_model=model, use_randomized_svd=True)
+        get_V_dict_for_TinyMNIST(OG_model=model, use_randomized_svd=False)
+        get_V_dict_for_TinyMNIST(OG_model=model, use_randomized_svd=True)
         return
 
     def test_num_dict_elts(self):
@@ -45,7 +66,7 @@ class TestLinearLayersBasic(unittest.TestCase):
         for use_randomized_svd in [True, False]:
             for hidden_sizes in [[10], [10, 5], [10, 5, 2]]:
                 model = TinyMNISTClassifier(input_size=64, hidden_sizes=hidden_sizes)
-                V_dict = get_V_dict_for_tests(
+                V_dict = get_V_dict_for_TinyMNIST(
                     OG_model=model, use_randomized_svd=use_randomized_svd
                 )
                 self.assertEqual(
@@ -61,7 +82,7 @@ class TestLinearLayersBasic(unittest.TestCase):
         """
         for hidden_sizes in [[10], [10, 5], [10, 5, 2]]:
             model = TinyMNISTClassifier(input_size=64, hidden_sizes=hidden_sizes)
-            V_dict = get_V_dict_for_tests(OG_model=model, use_randomized_svd=False)
+            V_dict = get_V_dict_for_TinyMNIST(OG_model=model, use_randomized_svd=False)
             for name, module in model.named_modules():
                 if hasattr(module, "weight") and (module.weight.grad is not None):
                     self.assertEqual(
@@ -74,6 +95,18 @@ class TestLinearLayersBasic(unittest.TestCase):
                         module.weight.numel(),
                         f"V_dict[{name}] does not have shape {module.weight.numel()}x{module.weight.numel()}",
                     )
+        return
+
+
+class TestCNNLayersNoLinearReparam(unittest.TestCase):
+    def test_computation_executes_no_linear_reparam(self):
+        """
+        Tests that we can run compute_V_by_layer without error
+        """
+        model = TinyCifarClassifier()
+        get_V_dict_for_Cifar10(
+            OG_model=model, use_randomized_svd=False, reparam_linear_layers=False
+        )
         return
 
 
