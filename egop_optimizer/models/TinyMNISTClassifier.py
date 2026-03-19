1d85f9c2464e6f12c251db63d53d6e4e03360663
Adela DePavia <adela.depavia@yale.edu>
2026-03-19 15:33:53 -0500

    Added ReparamTinyMNISTClassifier and updated test files to include this model

--- egop_optimizer/models/TinyMNISTClassifier.py
+++ egop_optimizer/models/TinyMNISTClassifier.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from egop_optimizer.models.BaseClassifier import BaseClassifier
+from egop_optimizer.models.BaseClassifier import BaseClassifier, ReparamBaseClassifier
+from egop_optimizer.models.reparam_layers.reparam_layers import EGOP_linear_layer
 
 import pdb
 
@@ -20,7 +21,9 @@ class TinyMNISTClassifier(BaseClassifier):
         None: Initializes the network layers dynamically based on hidden_sizes.
     """
 
-    def __init__(self, input_size=64, hidden_sizes=[32, 16], num_classes=10, seed=None):
+    def __init__(
+        self, input_size=64, hidden_sizes=[32, 16], num_classes=10, bias=True, seed=None
+    ):
         """
         Initializes the tinyMNIST classifier architecture.
 
@@ -38,12 +41,74 @@ class TinyMNISTClassifier(BaseClassifier):
         # Dynamically create fully connected layers
         current_input_size = input_size
         for i, hidden_size in enumerate(hidden_sizes):
-            setattr(self, f"fc{i+1}", nn.Linear(current_input_size, hidden_size))
+            # Uses bias by default
+            setattr(
+                self, f"fc{i+1}", nn.Linear(current_input_size, hidden_size, bias=bias)
+            )
             current_input_size = hidden_size
 
         # Final output layer
         setattr(
             self, f"fc{len(hidden_sizes)+1}", nn.Linear(current_input_size, num_classes)
+        )
+
+    def forward(self, x):
+        """
+        Performs a forward pass through the classifier.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (batch_size, *) compatible with input_size.
+
+        Returns:
+            torch.Tensor: Logits of shape (batch_size, num_classes).
+        """
+        x = self.flatten(x)
+        for i in range(1, len(self._modules)):
+            x = getattr(self, f"fc{i}")(x)
+            if i < len(self._modules) - 1:
+                x = F.relu(x)
+        return x
+
+
+class ReparamTinyMNISTClassifier(ReparamBaseClassifier):
+    def __init__(
+        self,
+        V_by_layer_dict,
+        input_size=64,
+        hidden_sizes=[32, 16],
+        num_classes=10,
+        seed=None,
+        bias=True,
+    ):
+        super().__init__(V_by_layer_dict=V_by_layer_dict, seed=seed)
+        self.flatten = nn.Flatten()
+
+        # Dynamically create fully connected layers
+        current_input_size = input_size
+        for i, hidden_size in enumerate(hidden_sizes):
+            layer_name = f"fc{i+1}"
+            setattr(
+                self,
+                f"fc{i+1}",
+                EGOP_linear_layer(
+                    V=V_by_layer_dict[f"fc{i+1}"],
+                    in_features=current_input_size,
+                    out_features=hidden_size,
+                    bias=bias,
+                ),
+            )
+            current_input_size = hidden_size
+
+        # Final output layer
+        setattr(
+            self,
+            f"fc{len(hidden_sizes)+1}",
+            EGOP_linear_layer(
+                V=V_by_layer_dict[f"fc{len(hidden_sizes)+1}"],
+                in_features=current_input_size,
+                out_features=num_classes,
+                bias=bias,
+            ),
         )
 
     def forward(self, x):
--- tests/test_ReparamBaseClassifier.py
+++ tests/test_ReparamBaseClassifier.py
@@ -6,6 +6,10 @@ from egop_optimizer.models.LinearFeedforward import (
     LinearFeedforward,
     ReparamLinearFeedforward,
 )
+from egop_optimizer.models.TinyMNISTClassifier import (
+    TinyMNISTClassifier,
+    ReparamTinyMNISTClassifier,
+)
 from egop_optimizer.dataloaders.tinyMNIST_dataloader import tinyMNIST_dataloader
 from egop_optimizer.utils.EGOP_utils import compute_V_by_layer
 from egop_optimizer.utils.device_utils import get_available_device
@@ -17,14 +21,16 @@ import pdb
 
 DEVICE = get_available_device()
 
-_DEFAULT_NETWORK_PARAMS = {
+_DEFAULT_LINEARFEEDFORWARD_PARAMS = {
     "input_size": 64,
     "output_size": 10,
     "hidden_sizes": [10, 5],
 }
 
 """
-TODO: Switch over to using linear_networks_dataloader instead of tinyMNIST
+TODO:
+-test with CNN and ResNet blocks
+-sanity check ranks of EGOP matrices
 """
 
 
@@ -39,6 +45,26 @@ def setUpModule():
         None: Writes the file path to stdout.
     """
     print(f"\nRunning tests in {__file__}")
+
+
+def get_V_dict_for_LinearFeedforward(OG_model, use_randomized_svd):
+    """
+    Instantiates dataloaders, criterion, and runs compute_V_by_layer
+    """
+    trainloader, _, _ = linear_networks_dataloader(
+        batch_size=128,
+        input_size=_DEFAULT_LINEARFEEDFORWARD_PARAMS["input_size"],
+        output_size=_DEFAULT_LINEARFEEDFORWARD_PARAMS["output_size"],
+    )
+    criterion = torch.nn.MSELoss(reduction="mean")
+
+    return compute_V_by_layer(
+        model_OG=OG_model,
+        k=100,
+        data_loader=trainloader,
+        criterion=criterion,
+        use_randomized_svd=use_randomized_svd,
+    )
 
 
 def get_V_dict_for_TinyMNIST(OG_model, use_randomized_svd):
@@ -57,7 +83,7 @@ def get_V_dict_for_TinyMNIST(OG_model, use_randomized_svd):
     )
 
 
-class TestBasicSetup(unittest.TestCase):
+class TestLinearFeedforwardReparam(unittest.TestCase):
     def test_initialization(self):
         """
         Instantiates a reparameterized model to verify successful initialization.
@@ -68,10 +94,12 @@ class TestBasicSetup(unittest.TestCase):
         Returns:
             None: Raises an exception if model construction fails.
         """
-        OG_model = LinearFeedforward(**_DEFAULT_NETWORK_PARAMS)
-        V_dict = get_V_dict_for_TinyMNIST(OG_model=OG_model, use_randomized_svd=False)
+        OG_model = LinearFeedforward(**_DEFAULT_LINEARFEEDFORWARD_PARAMS)
+        V_dict = get_V_dict_for_LinearFeedforward(
+            OG_model=OG_model, use_randomized_svd=False
+        )
         reparam_model = ReparamLinearFeedforward(
-            V_by_layer_dict=V_dict, **_DEFAULT_NETWORK_PARAMS
+            V_by_layer_dict=V_dict, **_DEFAULT_LINEARFEEDFORWARD_PARAMS
         )
 
     def test_model_eval(self, batch_size=128):
@@ -84,14 +112,43 @@ class TestBasicSetup(unittest.TestCase):
         Returns:
             None: Raises an exception if the forward pass fails.
         """
-        model = LinearFeedforward(**_DEFAULT_NETWORK_PARAMS)
-        model = model.to(DEVICE)
-        trainloader, _, _ = tinyMNIST_dataloader(batch_size=128)
+        OG_model = LinearFeedforward(**_DEFAULT_LINEARFEEDFORWARD_PARAMS)
+        V_dict = get_V_dict_for_LinearFeedforward(
+            OG_model=OG_model, use_randomized_svd=False
+        )
+        reparam_model = ReparamLinearFeedforward(
+            V_by_layer_dict=V_dict, **_DEFAULT_LINEARFEEDFORWARD_PARAMS
+        )
+        trainloader, _, _ = linear_networks_dataloader(
+            batch_size=128,
+            input_size=_DEFAULT_LINEARFEEDFORWARD_PARAMS["input_size"],
+            output_size=_DEFAULT_LINEARFEEDFORWARD_PARAMS["output_size"],
+        )
 
         train_iterator = iter(trainloader)
         Xb, yb = next(train_iterator)
 
-        y_hat = model(Xb)
+        y_hat = reparam_model(Xb)
+        return
+
+
+class TestTinyMNISTReparam:
+    def test_initialization(self):
+        OG_model = TinyMNISTClassifier()
+        V_dict = get_V_dict_for_TinyMNIST(OG_model=OG_model, use_randomized_svd=False)
+        reparam_model = ReparamLinearFeedforward(V_by_layer_dict=V_dict)
+        return
+
+    def test_model_eval(self, batch_size=128):
+        OG_model = TinyMNISTClassifier()
+        V_dict = get_V_dict_for_TinyMNIST(OG_model=OG_model, use_randomized_svd=False)
+        reparam_model = ReparamLinearFeedforward(V_by_layer_dict=V_dict)
+
+        trainloader, _, _ = tinyMNIST_dataloader(batch_size=128)
+        train_iterator = iter(trainloader)
+        Xb, yb = next(train_iterator)
+
+        y_hat = reparam_model(Xb)
         return
 
 
